ae9f14bacdc8c65147eff8855e9643e3cc9a43a4
Yashas Malleshappa <yashasm1991@gmail.com>
2016-09-24 21:38:49 -0700

    first commit

---
+++ public/angular/signin.js
@@ -0,0 +1,10 @@
+/**
+ * New node file
+ */
+
+var ebayApp = angular.module('ebayApp',['ngMaterial','ngAnimate','ngAria']);
+
+ebayApp.controller('signincontroller',['$scope',function($scope){
+	
+}]);
+
